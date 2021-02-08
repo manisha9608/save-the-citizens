6a6de769ecc01c6a72baf50d26a7f0bc9e5a7b88
manoja-m <78756147+manoja-m@users.noreply.github.com>
2021-02-09 00:14:52 +0530

    Update shortest-path.py

--- shortest-path.py
+++ shortest-path.py
@@ -36,82 +36,170 @@
     #5.3.	Print the total number of vertices (areas) visited by the agent in finding the path
     #Execute code to print the number of vertices travelled to cover the path. (using appropriate uninformed search)
 
-import numpy as np;
-
-INF = 1000000
-
-def floydWarshall(graph):
-    n = len(graph)
-
-    dist = [[] for i in range(n)]
 
 
-    # Initialize the dist matrix as same as the input graph∂
-    for i in range(n):
-        for j in range(n):
-            dist[i].append(graph[i][j])
+from collections import defaultdict 
 
-    # Calculate shortest path distance for each pair
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j])
+#Class to represent a graph 
+class Graph: 
+    
+    cost = 0
+    # A utility function to find the  
+    # vertex with minimum dist value, from 
+    # the set of vertices still in queue 
+    def minDistance(self,dist,queue): 
+        # Initialize min value and min_index as -1 
+        minimum = float("Inf") 
+        min_index = -1
 
-    # Shortest distance for every pair of vertex.
-    print('Shortest Distance between every pair of vertex:-')
-    for i in range(n):
-        for j in range(n):
-            if dist[i][j]==INF:
-                print ("%7s" % ("INF"),end=' ')
-            else:
-                print ("%7s" % (dist[i][j]),end=' ')
-        print()
-
-    return dist
-
-def main():
-    G = [[0, 110, 132, INF, INF, INF, INF],
-    [110, 0, INF, 159, INF, INF, 59],
-    [132, INF, 0, INF, 89, INF, 120],
-    [INF, 159, INF, 0, INF, 98, 108],
-    [INF, INF, 89, INF, 0, 68, 102],
-    [INF, INF, INF, 98, 68, 0, 92],
-    [INF, 59, 120, 108, 102, 92, 0]]
-    # print(G)
-
-    cities = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    start = 0
-    no_of_people = 0
-    dest_cities = []
-    unique_cities = {}
+        # from the dist array,pick one which 
+        # has min value and is till in queue 
+        for i in range(len(dist)): 
+            if dist[i] < minimum and i in queue: 
+                minimum = dist[i] 
+                min_index = i 
+        return min_index 
 
 
-    try:
-        start = cities.index(str(input('Enter start city: ')).upper()[0])
-        print('Start city is: ', cities[start])
+    # Function to get shortest path 
+    # from source to j 
+    # using parent array 
+    def addPath(self, parent, j): 
+        #Base Case : If j is source 
+        if parent[j] == -1 :  
+            return
+        self.addPath(parent , parent[j])
+        if parent[j] != -1:
+            sol_path.append(j)
+            
+    # Function to get cost 
+    # from source to j 
+    # using dist array
+    def addCost(self,dist,j):
+        g.cost = g.cost + dist[j]
 
-        no_of_people = int(input('Enter the no. of people on the ship: '))
-        # print(no_of_people)
+    '''Function that implements Dijkstra's single source shortest path 
+    algorithm for a graph represented using adjacency matrix 
+    representation'''
+    def dijkstra(self, graph, src): 
 
-        for i in range(0, no_of_people):
-            msg = "Enter destination city for person " + str(i) + ": "
-            # print(msg)
-            dest_cities.append(cities.index(str(input(msg)).upper()[0]))
-        print(dest_cities)
-    except:
-        print('Unexpected input!')
+        row = len(graph) 
+        col = len(graph[0]) 
 
-    reqd_cities = [city for city in dest_cities]
-    reqd_cities.extend([start])
-    # print(reqd_cities)
-    # print(dest_cities)
-    unique_cities = sorted(set(reqd_cities))
-    print(unique_cities)
-    dist = floydWarshall(G)
+        # The output array. dist[i] will hold 
+        # the shortest distance from src to i 
+        # Initialize all distances as INFINITE  
+        dist = [float("Inf")] * row 
 
-    # new_g =
+        #Parent array to store  
+        # shortest path tree 
+        parent = [-1] * row 
+
+        # Distance of source vertex  
+        # from itself is always 0 
+        dist[src] = 0
+
+        # Add all vertices in queue 
+        queue = [] 
+        for i in range(row): 
+            queue.append(i) 
+
+        #Find shortest path for all vertices 
+        while queue: 
+
+            # Pick the minimum dist vertex  
+            # from the set of vertices 
+            # still in queue 
+            u = self.minDistance(dist,queue)  
+
+            # remove min element      
+            queue.remove(u) 
+
+            # Update dist value and parent  
+            # index of the adjacent vertices of 
+            # the picked vertex. Consider only  
+            # those vertices which are still in 
+            # queue 
+            for i in range(col): 
+                '''Update dist[i] only if it is in queue, there is 
+                an edge from u to i, and total weight of path from 
+                src to i through u is smaller than current value of 
+                dist[i]'''
+                if graph[u][i] and i in queue: 
+                    if dist[u] + graph[u][i] < dist[i]: 
+                        dist[i] = dist[u] + graph[u][i] 
+                        parent[i] = u 
 
 
+        # print the constructed distance array
+        #print(dist)
+        mini = float("Inf")
+        min_dest = -1
+        
+        #Choose the nearest Destination to the source
+        for i in dest_cities:
+            if dist[i]<mini:
+                mini = dist[i]
+                min_dest = i
+        
+        if min_dest == -1:
+            return -1
+        else:
+            dest_cities.remove(min_dest)
+            self.addCost(dist,min_dest)
+            self.addPath(parent,min_dest)
+            return min_dest
+
+g= Graph() 
+#G = [[0, 110, 132, INF, INF, INF, INF],
+#    [110, 0, INF, 159, INF, INF, 59],
+#    [132, INF, 0, INF, 89, INF, 120],
+#    [INF, 159, INF, 0, INF, 98, 108],
+#    [INF, INF, 89, INF, 0, 68, 102],
+#    [INF, INF, INF, 98, 68, 0, 92],
+#    [INF, 59, 120, 108, 102, 92, 0]]
+#
+
+graph = [[0, 110, 132, 0, 0, 0, 0],
+    [110, 0, 0, 159, 0, 0, 59],
+    [132, 0, 0, 0, 89, 0, 120],
+    [0, 159, 0, 0, 0, 98, 108],
+    [0, 0, 89, 0, 0, 68, 102],
+    [0, 0, 0, 98, 68, 0, 92],
+    [0, 59, 120, 108, 102, 92, 0]]
+
+cities = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+start = 0
+no_of_people = 0
+dest_cities = []
+unique_cities = {}
+sol_path = []
 
 
-main()
+try:
+    start = cities.index(str(input('Enter start city: ')).upper()[0])
+    print('Start city is: ', cities[start])
+    sol_path.append(start)
+    no_of_people = int(input('Enter the no. of people on the ship: '))
+    # print(no_of_people)
+
+    for i in range(0, no_of_people):
+        msg = "Enter destination city for person " + str(i) + ": "
+        # print(msg)
+        dest_cities.append(cities.index(str(input(msg)).upper()[0]))
+    #print(dest_cities)
+except:
+    print('Unexpected input!')
+
+while dest_cities:
+    if start != -1:
+        start=g.dijkstra(graph,start)
+    else:
+        break
+    
+if start != -1:
+    print("The Shortest path is ")
+    for i in sol_path: print(" ", cities[i], end = " ") 
+    print("\nCost of Path is ", g.cost)
+else:
+    print("Path does not exist")
